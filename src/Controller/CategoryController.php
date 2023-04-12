2bab44184ca82556a751f9e6ae4edc38538d832d
BesokDev <jonathan.gredler@gmail.com>
2023-04-12 14:12:17 +0200

    Done

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -67,6 +67,37 @@ class CategoryController extends AbstractController
             'form' => $form->createView(),
             'category' => $category
         ]);
-    }
+    } // end update()
+
+    #[Route('/archiver-une-category/{id}', name: 'soft_delete_category', methods: ['GET'])]
+    public function softDeleteCategory(Category $category, CategoryRepository $repository): Response
+    {
+        $category->setDeletedAt(new Datetime());
+
+        $repository->save($category, true);
+
+        $this->addFlash('success', "La catégorie ". $category->getName() ." a bien été archivé.");
+        return $this->redirectToRoute('show_dashboard');
+    } // end softDelete()
+
+    #[Route('/restaurer-une-categorie/{id}', name: 'restore_category', methods: ['GET'])]
+    public function restoreCategory(Category $category, CategoryRepository $repository): Response
+    {
+        $category->setDeletedAt(null);
+
+        $repository->save($category, true);
+
+        $this->addFlash('success', "La catégorie ". $category->getName() ." a bien été restauré.");
+        return $this->redirectToRoute('show_dashboard');
+    } // end restore()
+
+    #[Route('/supprimer-une-categorie/{id}', name: 'hard_delete_category', methods: ['GET'])]
+    public function hardDeleteCategory(Category $category, CategoryRepository $repository): Response
+    {
+        $repository->remove($category, true);
+
+        $this->addFlash('success', "La catégorie a bien été supprimé définitivement.");
+        return $this->redirectToRoute('show_dashboard');
+    } // end hardDelete()
 
 } // end class
