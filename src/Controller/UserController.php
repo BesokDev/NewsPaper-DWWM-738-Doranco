000ad725427060a8dc61c7d7f17b0726234e77dc
BesokDev <jonathan.gredler@gmail.com>
2023-04-14 16:44:24 +0200

    make profile + change password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Commentary;
 use App\Entity\User;
+use App\Form\ChangePasswordFormType;
 use App\Form\RegisterFormType;
 use App\Repository\UserRepository;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -44,5 +48,61 @@ class UserController extends AbstractController
         ]);
 
     } // end register()
+
+    #[Route('/profile/voir-mon-compte', name: 'show_profile', methods: ['GET'])]
+    public function showProfile(EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Article::class)->findBy(['author' => $this->getUser()]);
+        $commentaries = $entityManager->getRepository(Commentary::class)->findBy(['author' => $this->getUser()]);
+
+        return $this->render('user/show_profile.html.twig', [
+            'articles' => $articles,
+            'commentaries' => $commentaries
+        ]);
+    } // end showProfile()
+    #[Route('/changer-mon-mot-de-passe', name: 'change_password', methods: ['GET', 'POST'])]
+    public function changePassword(Request $request, UserRepository $repository, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(ChangePasswordFormType::class)
+            ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            # Récupérer le User en BDD
+            $user = $repository->find($this->getUSer());
+
+            /* Listing des étapes :
+            1 - Un nouvel input => ChangePasswordFormType
+            2 - Récupérer la valeur de cet input
+            3 - Hasher le $currentPassword pour comparaison en BDD
+            4 - Condition de vérification
+            5 - Si la condition est vérifiée, alors on exécute le code.
+            */
+
+            // ------------------- VERIFICATION DU MDP --------------------//
+            $currentPassword = $form->get('currentPassword')->getData();
+
+            if(! $hasher->isPasswordValid($user, $currentPassword)) {
+                $this->addFlash('warning', "Le mot de passe actuel n'est pas valide.");
+                return $this->redirectToRoute('show_profile');
+            }
+
+            $user->setUpdatedAt(new DateTime());
+
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            $user->setPassword($hasher->hashPassword($user, $plainPassword));
+
+            $repository->save($user, true);
+
+            $this->addFlash('success', "Votre mot de passe a bien été modifié !");
+            return $this->redirectToRoute('show_profile');
+
+        }
+
+        return $this->render('user/change_password_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
 } // end class
